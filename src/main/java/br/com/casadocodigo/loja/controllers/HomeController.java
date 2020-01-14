7bc26c1c644eee029097631438e7149f2e7658ca
José Luiz <luizsobrinho3000@hotmail.com>
2020-01-13 21:54:10 -0300

    Branch Homologacao Teste

--- src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -23,7 +23,7 @@ public class HomeController {
 		List<Produto> produtos = produtoDao.listar();
 		ModelAndView modelAndView = new ModelAndView("home");
 		modelAndView.addObject("produtos", produtos);
-		System.out.println("Alteração");
+		System.out.println("Olá Mundo da Branch Homologacao");
 		return modelAndView;
 	}
 }
