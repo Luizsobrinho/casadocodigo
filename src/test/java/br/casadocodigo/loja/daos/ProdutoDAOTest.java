3d1651cb3d17b139df6162c160d7fab6d73327f1
José Luiz dos Santos Sobrinho <luizsobrinho3000@hotmail.com>
2020-02-18 08:54:51 -0300

    Atualizando o Hibernate core

--- src/test/java/br/casadocodigo/loja/daos/ProdutoDAOTest.java
+++ src/test/java/br/casadocodigo/loja/daos/ProdutoDAOTest.java
@@ -5,18 +5,28 @@ import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.casadocodigo.loja.builders.ProdutoBuilder;
+import br.com.casadocodigo.loja.conf.JPAConfiguration;
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { JPAConfiguration.class, ProdutoDAO.class })
 public class ProdutoDAOTest {
-	
-	@Test
-	public void deveSomarTodosPrecosPorTipoLivro() {
 
-		ProdutoDAO produtoDAO = new ProdutoDAO();
+	@Autowired
+	private ProdutoDAO produtoDAO;
+
+	@Test
+	@Transactional
+	public void deveSomarTodosPrecosPorTipoLivro() {
 
 		List<Produto> livrosImpressos = ProdutoBuilder.newProduto(TipoPreco.IMPRESSO, BigDecimal.TEN).more(3)
 				.buildAll();
@@ -24,7 +34,7 @@ public class ProdutoDAOTest {
 
 		livrosImpressos.stream().forEach(produtoDAO::gravar);
 		livrosEbook.stream().forEach(produtoDAO::gravar);
-		
+
 		BigDecimal valor = produtoDAO.somaPrecosPorTipoPreco(TipoPreco.EBOOK);
 		Assert.assertEquals(new BigDecimal(40).setScale(2), valor);
 	}
